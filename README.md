a336c562e8764a36dbf9b63e814596a7c3677f4a
root <root@ip-10-1-32-105.ec2.internal>
2021-05-18 08:58:03 +0000

    updated the python code README.md file

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Git_python
 
 this repo is having python script to get the github repo to local machine
-all commit changes will be copied to gotlog.txt file 
+all commit changes will be copied to gotlog.txt file
+
+command to redirect the terminal output a file called output.txt
+
+  python3 sample.py  2>&1 | tee output.txt 
--- sample.py
+++ sample.py
@@ -5,4 +5,4 @@ def subprocess_cmd(command):
     proc_stdout = process.communicate()[0].strip()
     print(proc_stdout.decode())
 
-subprocess_cmd('echo working dir is; pwd; echo listing the dir files ; ls -l; echo creating a dir in /home/; mkdir /home/Git; ls -l /home/; cd /home/Git; git clone https://github.com/salvadihari/example-voting-app.git; cd example-voting-app; echo listing the files in repo; ls -l; echo redirecting the git log commits to gitlog.txt; git log > gitlog.txt; ls -l')
+subprocess_cmd('echo working dir is; pwd; echo listing the dir files ; ls -l; echo creating a Git dir in /home/; mkdir /home/Git;Git dir is created; ls -l /home/; now cd to Git dir ; cd /home/Git; cloning teh github repo ; git clone https://github.com/salvadihari/example-voting-app.git;cd to repo ; cd example-voting-app; echo listing the files in repo; ls -l; echo redirecting the git log commits to gitlog.txt; git log > gitlog.txt; now we can able to see the gitlog.txt file;ls -l')
