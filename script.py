f2940a33a03c195c5ea3ad71806af0604b93cd22
salvadihari <41267832+salvadihari@users.noreply.github.com>
2021-05-21 16:03:39 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,29 +2,26 @@ import subprocess
 import os
 
 # gitgub repo link
-git_clone_url = "git clone https://github.com/salvadihari/Git_python"
-git_log_file = "git log > gitlog.txt"
+git_clone_url = "git clone https://github.com/salvadihari/Git_python.git"
 
 
 def subprocess_cmd(command):
     subprocess.Popen(command)
-    subprocess.Popen(git_log_file)
 
 
 def git_dir():
 
-    get_split_url_dir = git_clone_url.split("/")[-1]
-    dir = os.getcwd() + "/" + get_split_url_dir
-    print("git cloned repo dir is :", dir)
+    get_split_url_dir = git_clone_url.split("/")
+    get_dir = get_split_url_dir[-1].split(".")[-2]
+    print("work_dir is: ", get_dir)
+    os.chdir(get_dir)
+    print("cd to work dir: ", os.getcwd())
 
-    os.chdir(get_split_url_dir)
+    output = subprocess.run('git log > gitlog.txt', shell=True)
+    print("output.txt file is generated with gitlog commits: ",output)
 
-    os.getcwd()
-    print(os.getcwd())
-
-
-
-
+    os.listdir()
+    print(os.listdir())
 
 
 subprocess_cmd(git_clone_url)
