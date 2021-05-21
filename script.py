6d9964ea8de6792955022610a6a8cef2f1a9c181
salvadihari <41267832+salvadihari@users.noreply.github.com>
2021-05-21 17:53:24 +0530

    Update script.py

--- script.py
+++ script.py
@@ -6,18 +6,23 @@ git_clone_url = "git clone https://github.com/salvadihari/Git_python.git"
 
 
 def subprocess_cmd(command):
-    subprocess.Popen(command)
+    subprocess.run(command)
 
 
 def git_dir():
 
     get_split_url_dir = git_clone_url.split("/")
+    print("",get_split_url_dir)
     get_dir = get_split_url_dir[-1].split(".")[-2]
     print("work_dir is: ", get_dir)
+    repo_dir = os.getcwd() + "\\" + get_dir
+  #  print("repo_dir:", repo_dir)
+   # print(os.getcwd())
     os.chdir(get_dir)
-    print("cd to work dir: ", os.getcwd())
+    print(os.getcwd())
 
-    output = subprocess.run('git log > gitlog.txt', shell=True)
+
+    output = subprocess.run('git log > abcd.txt', shell=True)
     print("output.txt file is generated with gitlog commits: ",output)
 
     os.listdir()
@@ -26,5 +31,3 @@ def git_dir():
 
 subprocess_cmd(git_clone_url)
 git_dir()
-
-
